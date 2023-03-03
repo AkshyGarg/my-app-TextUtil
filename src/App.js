399920b4b5e752d77f411027b4fdd7cce2ba1cc1
AkshyGarg <gargakshay063@gmail.com>
2023-03-03 21:28:59 +0530

    TextUtil added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar'
+import TextForm from './Components/TextForm'
+import About from './Components/About';
+import Alert from './Components/Alert';
+import React ,{useState} from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
+  const [Mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const togglemode = () =>{
+    if(Mode === 'light'){
+      setMode('dark');
+      document.title = 'TextUtil - Dark Mode';
+    }
+    else{
+      setMode('light');
+    document.title = 'TextUtil';
+    }
+    // setInterval(() => {
+    // document.title = 'TextUtil - Downloaded ';
+    // }, 1800);
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar title="Akshyy" aboutText = "About" mode={Mode} toggleMode={togglemode}/>
+    <Alert message="Good Moring Folks"></Alert>
+    <Router>
+    <div className="container">
+      <Routes>
+          {/* <Route path="/about" element={<About/>}/> */}
+          <Route path="/" element={<TextForm title="Example Textarea" heading="Enter text to analyze" />}/>
+      </Routes>
     </div>
+    </Router>
+    {/* <About/> */}
+    </>    
   );
 }
 
---
+++ src/Components/TextForm.js
@@ -0,0 +1,67 @@
+import React ,{useState} from 'react'
+
+export default function TextForm(props) {
+    
+    const [text, setText] = useState('');
+
+    const handleUppercase= ()=>{
+    console.log("Uppercase clicked");
+    let newText = text.toUpperCase();
+    setText(newText);
+    }
+
+    const handlelowercase= ()=>{
+        console.log("Lowercase clicked");
+        let newText = text.toLowerCase();
+        setText(newText);
+        }
+
+    // user text area m type kr para h aur remove krr para h
+    const handleonChange = (event)=>{
+        console.log("on chnage");
+        setText(event.target.value);
+    }
+
+    const clearText = ()=>
+    {
+        setText('');
+    }
+
+    const copyToClipboard = () =>{
+        var text = document.getElementById("myProject");
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const handleExtraSpaces = () =>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
+    // const countWords = (event) =>{
+    //     let tem = event.target.value;
+    //     setCount(text.length);
+    // }
+    // const [count, setCount] = useState(0);
+    // onInput={countWords}
+  return (
+    <>
+    <div>
+        <div className="mb-3">
+        <h1>{props.heading}</h1>
+        <label for="myProject" className="form-label">{props.title}</label>
+        <textarea className="form-control" id="myProject" value={text} onChange={handleonChange} rows="10" />
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUppercase}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handlelowercase}>Convert to lowercase</button>
+        <button className="btn btn-primary mx-2" onClick={clearText}>Clear Text</button>
+        <button className="btn btn-primary mx-2" onClick={copyToClipboard}>Copy Text</button>
+        <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove white space</button>
+    </div>
+    <div className="container my-3">
+        <h2>Your Text Summary</h2>
+        <p>You typed {text.split(" ").length} Words and {text.length} Characters</p>
+        <h3>Preview Of Your Text</h3>
+        <p>{text.length>0?text:"Enter Something to see preview"}</p>
+    </div>
+    </>
+  )
+}
